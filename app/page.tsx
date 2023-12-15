72f9bfaa81bbbf5dd3eac1b55a647b9ab11ec29b
red-mat <jesus.ing.rodriguez@gmail.com>
2023-12-14 23:44:10 -0300

    feat(home): add luisitana fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
 
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
@@ -15,7 +16,9 @@ export default function Page() {
           <div className={styles.shape} />
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
 
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
